f8f0d16b0645941d2731778e04f7a0ec75106811
Emil Edholm <bobby@verypublic.org>
2017-10-21 21:35:31 +0200

    Basic power controller

---
+++ src/main/kotlin/pub/edholm/eiscprest/web/PowerController.kt
@@ -0,0 +1,31 @@
+package pub.edholm.eiscprest.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import pub.edholm.eiscprest.InputQueue
+import pub.edholm.eiscprest.OutputQueue
+import pub.edholm.eiscprest.eiscp.ISCPCommand
+
+@RestController
+@RequestMapping("/power")
+class PowerController(private val outputQueue: OutputQueue,
+                      private val inputQueue: InputQueue) {
+
+  @GetMapping("/query")
+  fun getPowerStatus(): String {
+    outputQueue.put(ISCPCommand(command = "!1PWR", payload = "QSTN"))
+    return inputQueue.pop().toString()
+  }
+
+  @PostMapping("/on")
+  fun powerOn() {
+    outputQueue.put(ISCPCommand(command = "!1PWR", payload = "01"))
+  }
+
+  @PostMapping("/off")
+  fun powerOff() {
+    outputQueue.put(ISCPCommand(command = "!1PWR", payload = "00"))
+  }
+}
